b9240306730f044716d06c411693e2e934cad401
Franziska Marggraf <fmarggra@c4r5p5.42vienna.com>
2024-10-11 16:59:40 +0200

    4

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -21,29 +21,31 @@ Base *generate(void) {
 }
 
 void    identify(Base *p) {
-    try {
-        if (!p)
-            throw std::invalid_argument("NULL pointer");
-        if (dynamic_cast<A*>(p))
-            std::cout << "A" << std::endl;
-        else if (dynamic_cast<B*>(p))
-            std::cout << "B" << std::endl;
-        else if (dynamic_cast<C*>(p))
-            std::cout << "C" << std::endl;
-        else
-            std::cout << "Error: type not found" << std::endl;
+    if (!p) {
+        std::cout << "Error: Null pointer" << std::endl;;
+        return ;
     }
-    catch (const std::invalid_argument &e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Error: type not found" << std::endl;
 }
 
 void    identify(Base &p) {
-    identify(&p);
+    try {
+        identify(&p);
+    }
+    catch (const std::invalid_argument &e) {
+        std::cout << "Error: Null pointer" << e.what() << std::endl;
+    }
 }
 
 int main(void) {
-    Base *oA = new A();
+    Base *oA = new A;
     Base *oB = new B();
     Base *oC = new C();
 
