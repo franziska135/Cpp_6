1d0717f8186fac046f0240d391406964c8cbf990
Franziska Marggraf <fmarggra@c4r5p10.42vienna.com>
2024-10-11 18:27:28 +0200

    4

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -37,15 +37,22 @@ void    identify(Base *p) {
 
 void    identify(Base &p) {
     try {
-        identify(&p);
+        if (dynamic_cast<A*>(&p))
+            std::cout << "A" << std::endl;
+        else if (dynamic_cast<B*>(&p))
+            std::cout << "B" << std::endl;
+        else if (dynamic_cast<C*>(&p))
+            std::cout << "C" << std::endl;
+        else
+            std::cout << "Error" << std::endl;
     }
-    catch (const std::invalid_argument &e) {
-        std::cout << "Error: Null pointer" << e.what() << std::endl;
+    catch (const std::exception &e) {
+        std::cout << "Error" << e.what() << std::endl;
     }
 }
 
 int main(void) {
-    Base *oA = new A;
+    Base *oA = NULL;
     Base *oB = new B();
     Base *oC = new C();
 
@@ -62,11 +69,22 @@ int main(void) {
     std::cout << "Identification using references:" << std::endl;
 
     std::cout << "Should identify A: ";
-    identify(*oA);
+    if (oA)
+        identify(*oA);
+    else
+        std::cout << "Error: NULL pointer cannot be passed to reference-based identify" << std::endl;
+    
     std::cout << "Should identify B: ";
-    identify(*oB);
+    if (oB)
+        identify(*oB);
+    else
+        std::cout << "Error: NULL pointer cannot be passed to reference-based identify" << std::endl;
+    
     std::cout << "Should identify C: ";
-    identify(*oC);
+    if (oC)
+        identify(*oC);
+    else
+        std::cout << "Error: NULL pointer cannot be passed to reference-based identify" << std::endl;
 
     if (oA)
         delete oA;
